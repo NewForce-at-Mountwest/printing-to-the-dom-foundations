d3ef724b58982fcf97ac2e19a24d2d11754dae0c
Jordan Castelloe <jordan.castelloe@gmail.com>
2021-09-16 18:04:24 -0400

    added example code for printing to the dom

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("This should show up in the dev tools")
+
+let myName = "Jordan"
+
+let h2Element = document.querySelector(".name-heading")
+console.log(h2Element)
+
+h2Element.innerHTML = "What's up Edgar"
